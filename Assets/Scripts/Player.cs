5170fc539d6b3d528c707b92bafefb9b7aae60ac
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-01-25 22:40:09 +0100

    Patrol scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float lookRange;
+    public float lookSphereCastRadius;
+    public float searchingTurnSpeed;
+    public float searchDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour
+{
+
+    public State currentState;
+    public Transform eyes;
+    public State remainState;
+    public Player player;
+    [HideInInspector] public int destPoint = 0;
+    public List<Transform> wayPointList;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElapsed;
+
+    public bool aiActive;
+    [Range(0, 2)]
+    public float speed;
+
+    internal bool CheckIfCountDownElapsed(object searchDuration)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Update()
+    {
+        if (!aiActive)
+            return;
+        currentState.UpdateState(this);
+    }
+
+    public void GotoNextPoint()
+    {
+        if (wayPointList.Count == 0)
+            return ;
+
+        if (destPoint >= wayPointList.Count)
+            destPoint = 0;
+        else
+            destPoint += 1;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+        }
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainState)
+        {
+            currentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool CheckIfCountDownElapsed(float duration)
+    {
+        stateTimeElapsed += Time.deltaTime;
+        return (stateTimeElapsed >= duration);
+    }
+
+    private void OnExitState()
+    {
+        stateTimeElapsed = 0;
+    }
+}
