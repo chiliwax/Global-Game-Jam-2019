acdbda986798c192bb0ad6bf9a7a04b970fda720
chiliwa <llecointe.thibault@gmail.com>
2019-01-26 10:26:15 +0100

    maid update

---
+++ Assets/Scripts/MaidAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaidAnimation : MonoBehaviour
+{
+    public Animator animator = null;
+    private float x;
+    private float y;
+
+    void start(){
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+    }
+    void Update()
+    {
+        ///ANIMATOR     
+        if (Mathf.Abs(this.transform.position.x - x) > Mathf.Abs(this.transform.position.y - y)) {
+        if (this.transform.position.x > x )
+            animator.SetInteger("Way", 1);
+        else if (this.transform.position.x < x )
+            animator.SetInteger("Way", 3);
+        }
+        else if (Mathf.Abs(this.transform.position.x - x) < Mathf.Abs(this.transform.position.y - y)) {
+        if (this.transform.position.y > y)
+            animator.SetInteger("Way", 4);
+        else if (this.transform.position.y < y)
+            animator.SetInteger("Way", 2);
+        }
+        else
+            animator.SetInteger("Way", 0);
+
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+        ///  
+    }
+}
